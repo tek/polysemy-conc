fd5e7bf52d0ac95c7e886e9e080473c86b36279e
Torsten Schmits <torstenschmits@gmail.com>
2021-07-01 23:58:10 +0200

    add 9.0.1

--- flake.nix
+++ flake.nix
@@ -1,14 +1,26 @@
 {
   description = "Polysemy Effects for Concurrency";
 
-  inputs.hix.url = github:tek/hix;
+  inputs = {
+    hix.url = github:tek/hix;
+    polysemy.url = github:polysemy-research/polysemy;
+  };
 
-  outputs = { hix, ... }:
+  outputs = { hix, polysemy, ... }:
   let
+    compat901 = { hackage, source, jailbreak, minimal, noHpack, ... }: {
+      path = hackage "0.9.0" "14symzl1rszvk5zivv85k79anz7xyl5gaxy0sm4vhhzsgxc59msv";
+      path-io = jailbreak (hackage "1.6.3" "05hcxgyf6kkz36mazd0fqwb6mjy2049gx3vh8qq9h93gfjkpp2vc");
+      polysemy = noHpack (minimal (source.root polysemy));
+      polysemy-plugin = noHpack (minimal (source.sub polysemy "polysemy-plugin"));
+      relude = hackage "1.0.0.1" "164p21334c3pyfzs839cv90438naxq9pmpyvy87113mwy51gm6xn";
+      type-errors-pretty = jailbreak;
+    };
+
     common = { hackage, only, ... }: {
-      polysemy-test = hackage "0.3.1.1" "0x0zg1kljr7a1mwmm3zrmha5inz3l2pkldnq65fvsig8f3x8rsar";
-      polysemy-time = hackage "0.1.2.1" "09l8r5fx0vnapdn8p0cwiwprgg3i67m58dd4j5hcdhw34gfqnnsr";
-      polysemy-chronos = hackage "0.1.2.1" "0layan6jxg857n2dmxwnylichnk2ynlpxih5iya3q8x2nbndpbl2";
+      polysemy-test = hackage "0.3.1.4" "093vxf6i78a3fghn2fwgxdj9y59272q9i9px0315wg17xrg80kh6";
+      polysemy-time = hackage "0.1.2.3" "039yfmpmy2d7ycs3zdcvg3hw80yrnbmwzfn9rh5a7p9gi4wzdcgd";
+      polysemy-chronos = hackage "0.1.2.3" "1sccjkb8s50xycvsl3xb8blgilmhfb0xfh6jgsq46k22j7mzgnyc";
     };
 
     main = { hackage, ... }: {
@@ -18,15 +30,11 @@
       polysemy-plugin = hackage "0.3.0.0" "1frz0iksmg8bpm7ybnpz9h75hp6hajd20vpdvmi04aspklmr6hj0";
       relude = hackage "1.0.0.1" "164p21334c3pyfzs839cv90438naxq9pmpyvy87113mwy51gm6xn";
     };
-
-    compat = { hackage, source, only, ... }: {
-      polysemy = only "865" (hackage "1.4.0.0" "04bl0w7z35jh63jpy87sa1rrbgqhwn7c0pxsm5l3ww0pjnswkhjj");
-    };
   in
   hix.flake {
     base = ./.;
     overrides = [common main];
-    compatOverrides = [common compat];
+    compatOverrides = { all = common; ghc901 = compat901; };
     packages.polysemy-conc = "packages/conc";
     ghci.extraArgs = ["-fplugin=Polysemy.Plugin"];
     versionFile = "ops/hpack/shared/meta.yaml";
