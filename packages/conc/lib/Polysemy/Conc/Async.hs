2ea9943e7a6f8622d7b9062fb5ba73a5083d76b1
Torsten Schmits <torstenschmits@gmail.com>
2021-08-07 01:02:31 +0200

    doc for withAsync

--- packages/conc/lib/Polysemy/Conc/Async.hs
+++ packages/conc/lib/Polysemy/Conc/Async.hs
@@ -3,6 +3,7 @@ module Polysemy.Conc.Async where
 import Polysemy.Async (Async, async, cancel)
 import Polysemy.Resource (Resource, bracket)
 
+-- |Run the first action asynchronously while the second action executes, then cancel the first action.
 withAsync ::
   Members [Resource, Async] r =>
   Sem r b ->
