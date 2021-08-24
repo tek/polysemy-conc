9e9afa6090f080316e75de9dc01205ca2d2d85d0
Torsten Schmits <torstenschmits@gmail.com>
2021-08-24 23:41:35 +0200

    change a name

--- packages/conc/lib/Polysemy/Conc/Effect/Scoped.hs
+++ packages/conc/lib/Polysemy/Conc/Effect/Scoped.hs
@@ -44,17 +44,17 @@ scoped main =
 -- |Interpreter for 'Scoped', taking a @resource@ allocation function and a parameterized interpreter for the plain
 -- @effect@.
 --
--- @scope@ is a callback function, allowing the user to compute the resource for each program from other effects.
+-- @withResource@ is a callback function, allowing the user to acquire the resource for each program from other effects.
 --
 -- @scopedInterpreter@ is a regular interpreter that is called with the @resource@ argument produced by @scope@.
 -- /Note/: This function will be called for each action in the program, so if the interpreter allocates any resources,
--- they will scoped to a single action. Move them to @scope@ instead.
+-- they will be scoped to a single action. Move them to @withResource@ instead.
 runScoped ::
   ∀ resource effect r .
   (∀ x . (resource -> Sem r x) -> Sem r x) ->
   (resource -> InterpreterFor effect r) ->
   InterpreterFor (Scoped resource effect) r
-runScoped scope scopedInterpreter =
+runScoped withResource scopedInterpreter =
   run
   where
     run :: InterpreterFor (Scoped resource effect) r
@@ -62,8 +62,8 @@ runScoped scope scopedInterpreter =
       interpretH' \ (Weaving effect s wv ex ins) -> case effect of
         Run resource act ->
           scopedInterpreter resource (liftSem $ injWeaving $ Weaving act s (raise . run . wv) ex ins)
-        InScope main -> do
-          ex <$> scope \ resource -> run (wv (main resource <$ s))
+        InScope main ->
+          ex <$> withResource \ resource -> run (wv (main resource <$ s))
 
 -- |Variant of 'Scoped' in which the resource allocator is a plain action.
 runScopedAs ::
