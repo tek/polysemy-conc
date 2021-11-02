802929f9483a3c168ee6addbc2bcde71330386c4
Torsten Schmits <git@tryp.io>
2021-11-02 01:54:31 +0100

    fix compat deps

--- flake.nix
+++ flake.nix
@@ -5,33 +5,27 @@
 
   outputs = { hix, ... }:
   let
-    common = { hackage, source, jailbreak, ... }: {
-      path = hackage "0.9.0" "14symzl1rszvk5zivv85k79anz7xyl5gaxy0sm4vhhzsgxc59msv";
-      path-io = jailbreak (hackage "1.6.3" "05hcxgyf6kkz36mazd0fqwb6mjy2049gx3vh8qq9h93gfjkpp2vc");
+
+    compat901 = { jailbreak, ... }: {
+      type-errors-pretty = jailbreak;
+    };
+
+    compat = { hackage, ... }: {
+      polysemy-resume = hackage "0.2.0.0" "0kh7cwqkr5w69zkm68l6q4d8nkai7fc29n48p3f8skqw638x4w9p";
+      polysemy-test = hackage "0.3.1.7" "0j33f5zh6gyhl86w8kqh6nm02915b4n32xikxc4hwcy7p5l7cl34";
+      polysemy-time = hackage "0.1.3.2" "1s06c1jwsq9ckfcq2cwwpiy5a2a0lj8j63zg4jr2kidpd2lkk6cd";
+    };
+
+    all = { hackage, only, unbreak, jailbreak, super, ... }: {
       polysemy = hackage "1.6.0.0" "15k51ysrfcbkww1562g8zvrlzymlk2rxhcsz9ipsb0q6h571qgvf";
       polysemy-plugin = hackage "0.4.1.0" "117g92l1ppsqd3w0rqjrxfk0lx6yndd54rpymgxljilnv43zg29s";
       relude = hackage "1.0.0.1" "164p21334c3pyfzs839cv90438naxq9pmpyvy87113mwy51gm6xn";
     };
 
-    compat901 = { hackage, source, jailbreak, minimal, noHpack, ... }: {
-      type-errors-pretty = jailbreak;
-    };
-
-    compat = { hackage, only, unbreak, jailbreak, super, ... }: {
-      polysemy = hackage "1.6.0.0" "15k51ysrfcbkww1562g8zvrlzymlk2rxhcsz9ipsb0q6h571qgvf";
-      polysemy-plugin = hackage "0.4.1.0" "117g92l1ppsqd3w0rqjrxfk0lx6yndd54rpymgxljilnv43zg29s";
-      polysemy-test = hackage "0.3.1.7" "0j33f5zh6gyhl86w8kqh6nm02915b4n32xikxc4hwcy7p5l7cl34";
-      polysemy-time = hackage "0.1.3.2" "1s06c1jwsq9ckfcq2cwwpiy5a2a0lj8j63zg4jr2kidpd2lkk6cd";
-    };
-
-    main = { hackage, ... }: {
-      tasty-hedgehog = hackage "1.1.0.0" "0cs96s7z5csrlwj334v8zl459j5s4ws6gmjh59cv01wwvvrrjwd9";
-    };
-  in
-  hix.flake {
+  in hix.flake {
     base = ./.;
-    overrides = [compat common main];
-    compatOverrides = { all = compat; ghc901 = [common compat901]; };
+    overrides = all;
+    compatOverrides = { all = all; ghc901 = compat901; ghc884 = compat; ghc8107 = compat; };
     packages = {
       polysemy-conc = ./packages/conc;
       polysemy-process = ./packages/process;
