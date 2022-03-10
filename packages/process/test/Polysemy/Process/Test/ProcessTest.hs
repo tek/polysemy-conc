5bdf3b9146df479d2380eba42964b7b5b5d77970
Torsten Schmits <git@tryp.io>
2022-03-10 21:43:29 +0100

    add timeout to Process tests

--- packages/process/test/Polysemy/Process/Test/ProcessTest.hs
+++ packages/process/test/Polysemy/Process/Test/ProcessTest.hs
@@ -2,9 +2,12 @@
 
 module Polysemy.Process.Test.ProcessTest where
 
+import Polysemy.Conc.Effect.Scoped (Scoped)
 import Polysemy.Conc.Interpreter.Race (interpretRace)
+import qualified Polysemy.Conc.Race as Race
 import Polysemy.Resume (resumeHoistError)
 import Polysemy.Test (UnitTest, runTestAuto, (===))
+import Polysemy.Time (Seconds (Seconds))
 import qualified System.Process.Typed as Process
 import System.Process.Typed (ProcessConfig)
 
@@ -28,17 +31,17 @@ message =
 test_process :: UnitTest
 test_process =
   runTestAuto $ interpretRace $ asyncToIOFinal $ interpretProcessByteStringNative True 10 config do
-    response <- resumeHoistError @ProcessError show do
+    response <- resumeHoistError @ProcessError @(Scoped _ _) show do
       withProcess do
         Process.send message
-        Process.recv
+        Race.timeout_ (throw "timed out") (Seconds 5) Process.recv
     message === response
 
 test_processLines :: UnitTest
 test_processLines =
   runTestAuto $ interpretRace $ asyncToIOFinal $ interpretProcessTextLinesNative True 10 config do
-    response <- resumeHoistError @ProcessError show do
+    response <- resumeHoistError @ProcessError @(Scoped _ _) show do
       withProcess do
         Process.send message
-        replicateM 4 Process.recv
+        Race.timeout_ (throw "timed out") (Seconds 5) (replicateM 4 Process.recv)
     messageLines === response
