16b4aadc28748f4d04d9631ebb926179ef6a6e48
Torsten Schmits <git@tryp.io>
2023-04-06 20:43:38 +0200

    bump hix

--- flake.nix
+++ flake.nix
@@ -1,42 +1,127 @@
 {
-  description = "Polysemy Effects for Concurrency";
+  description = "Polysemy effects for concurrency";
 
   inputs = {
-    hix.url = git+https://git.tryp.io/tek/hix;
-    polysemy-time.url = git+https://git.tryp.io/tek/polysemy-time?tag=v0.6.0.0;
+    hix.url = "git+https://git.tryp.io/tek/hix";
+    polysemy-time.url = "git+https://git.tryp.io/tek/polysemy-time";
   };
 
-  outputs = { hix, polysemy-time, ... }:
-  let
+  outputs = { hix, polysemy-time, ... }: hix.lib.pro {
+    ghcVersions = ["ghc810" "ghc90" "ghc92" "ghc94"];
+    hackage.versionFile = "ops/version.nix";
+    main = "polysemy-process";
+    deps = [polysemy-time];
 
-    dev = { hackage, source, ... }: {
-      polysemy = hackage "1.9.0.0" "1af07cppnjpv5v56wanya1mhkvbfnyynf5447mnkcf4zc4k23pyk";
-      polysemy-plugin = hackage "0.4.4.0" "08ry72bw78fis9iallzw6wsrzxnlmayq2k2yy0j79hpw4sp8knmg";
-    };
-
-    all = { hackage, ... }: {
-      incipit-base = hackage "0.4.1.0" "17579j3hzsh3ic0272h8ly8k7gz4zm1hv5jqimdam9gcq8alahkl";
-      incipit-core = hackage "0.4.1.0" "1fm6bf1w8mvpa9qlgxqv3ngf0lyb3057cwv5ajibgbljjaznxpxc";
+    overrides = { hackage, ... }: {
+      incipit-base = hackage "0.5.1.0" "0hkqnqpdw8rvg4xzslw9sp3684ggyk9n4hr0lczwm8b0pzakzs0l";
+      incipit-core = hackage "0.5.1.0" "04lyzycvqxyjqcd703cd33lnlk5va9wj3czpsybah0ybydnrwabd";
       polysemy = hackage "1.9.0.0" "1af07cppnjpv5v56wanya1mhkvbfnyynf5447mnkcf4zc4k23pyk";
       polysemy-plugin = hackage "0.4.4.0" "08ry72bw78fis9iallzw6wsrzxnlmayq2k2yy0j79hpw4sp8knmg";
       polysemy-resume = hackage "0.7.0.0" "1b9agh2qd0nrbd7cc5iabkzjb7g9lnzzy3pprvn33hr54va9p928";
       polysemy-time = hackage "0.6.0.0" "1ay0ym01wznk98km2ksw8slj52gc7rav6n16z4sndzsw7cdwdq2y";
     };
 
-  in hix.lib.pro ({ config, lib, ...}: {
-    packages = {
-      polysemy-conc = ./packages/conc;
-      polysemy-process = ./packages/process;
+    cabal = {
+      license = "BSD-2-Clause-Patent";
+      license-file = "LICENSE";
+      author = "Torsten Schmits";
+      prelude = {
+        enable = true;
+        package = {
+          name = "incipit-core";
+          version = "^>= 0.5";
+        };
+        module = "IncipitCore";
+      };
+      meta = {
+        maintainer = "hackage@tryp.io";
+        github = "tek/polysemy-conc";
+        extra-source-files = ["readme.md" "changelog.md"];
+      };
     };
-    main = "polysemy-process";
-    devGhc.compiler = "ghc902";
-    overrides = { inherit dev all; };
-    deps = [polysemy-time];
-    hpack.packages = import ./ops/hpack.nix { inherit config lib; };
-    hackage.versionFile = "ops/version.nix";
-    ghci = {
-      preludePackage = "incipit-core";
-      preludeModule = "IncipitCore";
+
+    packages.polysemy-conc = {
+      src = ./packages/conc;
+
+      cabal.meta.synopsis = "Polysemy effects for concurrency";
+      cabal.meta.category = "Concurrency";
+
+      library = {
+        enable = true;
+        dependencies = [
+          "async"
+          "containers"
+          "polysemy ^>= 1.9"
+          "polysemy-resume ^>= 0.7"
+          "polysemy-time ^>= 0.6"
+          "stm"
+          "stm-chans ^>= 3"
+          "torsor ^>= 0.1"
+          "unagi-chan ^>= 0.4"
+          "unix"
+        ];
+      };
+
+      test = {
+        enable = true;
+        dependencies = [
+          "async"
+          "hedgehog >= 1.1 && < 1.3"
+          "polysemy"
+          "polysemy-conc"
+          "polysemy-plugin ^>= 0.4.4"
+          "polysemy-test >= 0.6 && < 0.8"
+          "polysemy-time"
+          "stm"
+          "tasty ^>= 1.4"
+          "tasty-hedgehog >= 1.3 && < 1.5"
+          "time"
+          "unix"
+        ];
+      };
+
     };
-  });
+
+    packages.polysemy-process = {
+      src = ./packages/process;
+
+      cabal.meta.synopsis = "Polysemy effects for system processes";
+      cabal.meta.category = "Process";
+
+      library = {
+        enable = true;
+        dependencies = [
+          "path ^>= 0.9"
+          "path-io ^>= 1.7"
+          "polysemy ^>= 1.9"
+          "polysemy-conc ^>= 0.12"
+          "polysemy-resume ^>= 0.7"
+          "polysemy-time ^>= 0.6"
+          "posix-pty >= 0.2"
+          "process"
+          "stm-chans ^>= 3"
+          "typed-process ^>= 0.2.6"
+          "unix"
+        ];
+      };
+
+      test = {
+        enable = true;
+        dependencies = [
+          "polysemy"
+          "polysemy-conc"
+          "polysemy-plugin ^>= 0.4.4"
+          "polysemy-process"
+          "polysemy-resume"
+          "polysemy-test >= 0.6 && < 0.8"
+          "polysemy-time"
+          "tasty ^>= 1.4"
+          "tasty-expected-failure ^>= 0.12"
+          "typed-process"
+        ];
+      };
+
+    };
+
+  };
 }
